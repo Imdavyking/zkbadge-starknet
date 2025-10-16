e1baaeb5fc882bcf055c0b4873c619dd9aef2510
imdavyking <yungdynamic53@gmail.com>
2025-10-16 23:46:41 +0100

    chore: updaged

--- frontend/src/helpers/gen_proof.ts
+++ frontend/src/helpers/gen_proof.ts
@@ -46,7 +46,7 @@ export function useZkVerifier() {
     const execResult = await noir.execute(input);
 
     console.log("🔒 Generating proof...");
-    console.log("Witness size:", execResult.witness);
+    console.log("Witness:", execResult.witness);
     // Verify public inputs match
     console.log("Public inputs:", execResult.returnValue);
     const honk = new UltraHonkBackend(circuit.bytecode, { threads: 2 });
@@ -56,7 +56,7 @@ export function useZkVerifier() {
     });
     honk.destroy();
 
-    console.log("🧩 Preparing callData...");
+    console.log(`🧩 Preparing callData...${proof}`);
     await initGaraga();
     const callData = getHonkCallData(
       proof.proof,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import serveStatic from "vite-plugin-serve-static";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 // import rollupNodePolyFill from "rollup-plugin-polyfill-node";
@@ -12,6 +13,16 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
+    {
+      // Workaround for https://github.com/keep-starknet-strange/scaffold-garaga/issues/5
+      ...serveStatic([
+        {
+          pattern: /main.worker.js/,
+          resolve: "node_modules/@aztec/bb.js/dest/browser/main.worker.js",
+        },
+      ]),
+      apply: "serve", // Only apply in dev mode
+    },
     // wasm(),
     // nodePolyfills({
     //   include: ["process", "util", "path"],
