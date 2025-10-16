277391a93080194c0365d0c38c8403654d5ad11d
imdavyking <yungdynamic53@gmail.com>
2025-10-16 13:08:58 +0100

    chore: updated

--- circuit/poseidon.js
+++ circuit/poseidon.js
@@ -7,9 +7,16 @@ const valid_until = 1735689600n;
 const is_valid = 1n;
 const owner = BigInt("0x057524576a4cec4a2fc58b79b6be09de");
 const year_of_birth = 1990n;
+const secret = BigInt("0x27176fe12e4034d09b28fedd72ba97");
+const access_nullifier = BigInt("0x2fc58b7057524576a4cec4a9b6be09de");
 
 const fields = [issuer, issued_at, valid_until, is_valid, owner, year_of_birth];
 
 // Chain poseidon2 hashes just like in Cairo
 let hash = poseidon2Hash(fields);
-console.log("0x" + hash.toString(16));
+const data = {
+  cert_hash: "0x" + hash.toString(16),
+  access_nullifier_hash: "0x" + poseidon2Hash([hash, secret]).toString(16),
+};
+
+console.log(data);
